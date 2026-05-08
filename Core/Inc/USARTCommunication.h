ecd5041c44045af43cd04f3f08d1909770b1a14b
waweroit <waweroit@gmail.com>
2026-05-08 15:01:06 +0200

    Tighten USART communication interface

--- Core/Inc/USARTCommunication.h
+++ Core/Inc/USARTCommunication.h
@@ -9,9 +9,10 @@
 #define INC_USARTCOMMUNICATION_H_
 
 #include <stdbool.h>
-#include <string.h> // Potrzebne do strcmp, strcpy
+#include <stddef.h>
+#include <stdint.h>
+#include <stdarg.h>
 #include "usart.h"
-#include <stdarg.h>   // <-- to jest kluczowe
 
 #define Channel_USART &huart1
 
@@ -24,37 +25,13 @@ extern volatile uint8_t rxByte;
 
 extern uint8_t txBuffer[TX_BUFFER_SIZE];
 
+void InitUSART(void);
 
-void InitUSART();
+bool USART_TakeLine(char *out, size_t outSize);
+void USART_OnByteReceived(uint8_t b);
+void USART_StartReceiveIT(void);
 
-
-
-bool USART_TakeLine(char *out, size_t outSize);   // pobiera gotową linię (jeśli jest)
-void USART_OnByteReceived(uint8_t b);             // sklejanie linii (wołane z RX IRQ)
-void USART_StartReceiveIT(void);                  // restart pojedynczego RX
-
-
-//memset(txBuffer, 0, sizeof(txBuffer));
-//snprintf(txBuffer, sizeof(txBuffer), "Przyklad uzycia 1: %d %%\r\n", MoistureHumidityPercent_Dev_1);
-//Send(txBuffer);
 HAL_StatusTypeDef Sendf(const char *fmt, ...);
 HAL_StatusTypeDef Send(const char *text);
 
-// do dodania w MAIN !
-//#include <stdio.h>
-//void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-//{
-//    if (huart->Instance == USART1)
-//    {
-//
-//    }
-//}
-//
-//void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
-//{
-//    if (huart->Instance == USART1) // Sprawdź, czy to Twój UART
-//    {
-//        isTransmissionComplete = true;
-//    }
-//}
 #endif /* INC_USARTCOMMUNICATION_H_ */
